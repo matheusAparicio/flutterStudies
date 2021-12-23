881cd03f035402dc1398635ce78d2e3b742550be
matheusAparicio <matheusilva-m@outlook.com>
2021-12-23 18:00:30 -0300

    adicao lista de tarefas

--- lib/my_drawer.dart
+++ lib/my_drawer.dart
@@ -3,61 +3,90 @@ import 'package:estudos_flutter1/home_page.dart';
 import 'package:estudos_flutter1/person_counter.dart';
 import 'package:estudos_flutter1/imc_calculator.dart';
 import 'package:estudos_flutter1/currency_converter.dart';
-
+import 'package:estudos_flutter1/todo_list.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     void gotoHomePage() {
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => const HomePage()));
-  }
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomePage()));
+    }
 
     void gotoPersonCounter() {
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => const PersonCounter()));
-  }
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const PersonCounter()));
+    }
 
-  void gotoImcCalculator() {
-    Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (context) => const ImcCalculator()));
-  }
+    void gotoImcCalculator() {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const ImcCalculator()));
+    }
 
-  void gotoCurrencyConverter() {
-    Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (context) => const CurrencyConverter()));
-  }
+    Future<dynamic> gotoCurrencyConverter() {
+      return Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const CurrencyConverter()));
+    }
+
+    void gotoToDoList() {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const ToDoList()));
+    }
+
+    Widget buttonDrawer(
+        {String? buttonName,
+        IconData? buttonIcon,
+        Color? buttonColor,
+        required VoidCallback buttonPressed}) {
+      return TextButton.icon(
+          style: const ButtonStyle(alignment: Alignment.centerLeft),
+          onPressed: ()=> buttonPressed,
+          icon: Icon(buttonIcon, color: buttonColor),
+          label: Text(buttonName!, style: TextStyle(color: buttonColor)));
+    }
 
     return Drawer(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(40, 40, 60, 80),
-          child: ListView(
-            children: [
-              TextButton.icon(
-                  onPressed: gotoHomePage,
-                  icon: const Icon(Icons.home, color: Colors.black),
-                  label: const Text("Página inicial")),
-              const SizedBox(height: 45),
-              TextButton.icon(
-                  onPressed: gotoPersonCounter,
-                  icon: const Icon(Icons.person, color: Colors.black),
-                  label: const Text("Contador de Pessoas")),
-              const SizedBox(height: 10),
-              TextButton.icon(
-                  onPressed: gotoImcCalculator,
-                  icon: const Icon(Icons.calculate, color: Colors.black),
-                  label: const Text("Calculadora de IMC")),
-              const SizedBox(height: 10),
-              TextButton.icon(
-                  onPressed: gotoCurrencyConverter,
-                  icon: const Icon(Icons.money, color: Colors.black),
-                  label: const Text("Conversor de Moedas")),
-            ],
-          ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 40, 60, 80),
+        child: ListView(
+          children: [
+            buttonDrawer(
+                buttonName: "Página Inicial",
+                buttonColor: Colors.black,
+                buttonIcon: Icons.home,
+                buttonPressed: ()=>gotoHomePage()),
+            const SizedBox(height: 45),
+            TextButton.icon(
+                style: const ButtonStyle(alignment: Alignment.centerLeft),
+                onPressed: gotoPersonCounter,
+                icon: const Icon(Icons.person, color: Colors.black),
+                label: const Text("Contador de Pessoas")),
+            const SizedBox(height: 10),
+            TextButton.icon(
+                style: const ButtonStyle(alignment: Alignment.centerLeft),
+                onPressed: gotoImcCalculator,
+                icon: const Icon(Icons.calculate, color: Colors.black),
+                label: const Text("Calculadora de IMC")),
+            const SizedBox(height: 10),
+            TextButton.icon(
+                style: const ButtonStyle(alignment: Alignment.centerLeft),
+                onPressed: gotoCurrencyConverter,
+                icon: const Icon(Icons.money, color: Colors.black),
+                label: const Text("Conversor de Moedas",
+                    style: TextStyle(color: Colors.amber))),
+            const SizedBox(height: 10),
+            TextButton.icon(
+              style: const ButtonStyle(alignment: Alignment.centerLeft),
+              onPressed: gotoToDoList,
+              icon: const Icon(Icons.list, color: Colors.black),
+              label: Text("Lista de Tarefas",
+                  style: TextStyle(color: Colors.blue[800])),
+            ),
+          ],
         ),
-      );
+      ),
+    );
   }
 }
---
+++ lib/todo_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'my_drawer.dart';
+
+
+class ToDoList extends StatefulWidget {
+  const ToDoList({ Key? key }) : super(key: key);
+
+  @override
+  _ToDoListState createState() => _ToDoListState();
+}
+
+class _ToDoListState extends State<ToDoList> {
+  @override
+  Widget build(BuildContext context) {
+    // ignore: prefer_const_constructors
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lista de Tarefas"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.refresh, color: Colors.white))
+        ],
+        backgroundColor: Colors.blue[800],
+      ),
+      drawer: const MyDrawer(),
+      body: Center(
+        child: Column(children: const []),
+      ),
+    );
+  }
+}
