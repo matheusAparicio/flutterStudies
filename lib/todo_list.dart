84350222c6f2dfcf73b1b24047bd93a47aa5dfcd
matheusAparicio <matheusilva-m@outlook.com>
2022-01-24 17:51:57 -0300

    todo_list improved

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'my_drawer.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ToDoList extends StatefulWidget {
   const ToDoList({Key? key}) : super(key: key);
@@ -9,6 +14,38 @@ class ToDoList extends StatefulWidget {
 }
 
 class _ToDoListState extends State<ToDoList> {
+  final toDoMainColor = Colors.blue[800];
+
+  List _toDoList = [];
+
+  Map<String, dynamic> _lastRemoved = Map();
+
+  int? _lastRemovedPos;
+
+  TextEditingController taskController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
+  void _addToDo() {
+    setState(() {
+      Map<String, dynamic> newToDo = Map();
+      newToDo["title"] = taskController.text;
+      newToDo["ok"] = false;
+      taskController.text = "";
+      _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
@@ -21,15 +58,119 @@ class _ToDoListState extends State<ToDoList> {
               onPressed: () {},
               icon: const Icon(Icons.refresh, color: Colors.white))
         ],
-        backgroundColor: Colors.blue[800],
+        backgroundColor: toDoMainColor,
       ),
       drawer: const MyDrawer(),
-      body: Center(
-        child: Column(children: const [
-          Padding(padding: EdgeInsets.fromLTRB(0, 20, 0, 20)),
-          Text("Texto de teste"),
-        ]),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.fromLTRB(17, 1, 7, 1),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: TextField(
+                    controller: taskController,
+                    decoration: InputDecoration(
+                      labelText: "Nova Tarefa",
+                      labelStyle: TextStyle(
+                        color: toDoMainColor,
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(toDoMainColor),
+                    ),
+                    child: const Text("Add"),
+                    onPressed: _addToDo)
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: const EdgeInsets.only(top: 10),
+              itemCount: _toDoList.length,
+              itemBuilder: buildItem,
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Widget buildItem(context, index) {
+    return Dismissible(
+      key: Key(_toDoList[index]["title"]),
+      direction: DismissDirection.startToEnd,
+      background: Container(
+          color: Colors.redAccent,
+          child: const Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          )),
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        checkColor: Colors.white,
+        activeColor: toDoMainColor,
+        secondary: CircleAvatar(
+          backgroundColor: toDoMainColor,
+          child: Icon(
+            _toDoList[index]["ok"] ? Icons.check : Icons.error,
+            color: Colors.white,
+          ),
+        ),
+        onChanged: (c) {
+          setState(() {
+            _toDoList[index]["ok"] = c;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" foi removida."),
+            action: SnackBarAction(label: "Desfazer",
+            onPressed: () {
+              _toDoList.insert(_lastRemovedPos??0, _lastRemoved);
+              _saveData();
+            }),
+          );
+
+          ScaffoldMessenger.of(context).showSnackBar(snack);
+
+        });
+      },
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return "";
+    }
   }
 }
